a0a4887d88f746cd5a39951cf60388ca291d21f2
AndriiLupoi <lupoi_2005@icloud.com>
2025-10-06 15:03:21 +0300

    CSRF

--- src/main/java/org/lupoi/security25/config/SecurityConfig.java
+++ src/main/java/org/lupoi/security25/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package org.lupoi.security25;/*
+package org.lupoi.security25.config;/*
     @author user
     @project security25
     @class SecurityConfig
@@ -8,16 +8,16 @@ package org.lupoi.security25;/*
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.security.config.Customizer;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    //Поміняв конфігурацію тому що коли робиш DELETE, PUT, POST запити
-    //Кидає 401 тобто не авторизований.
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
